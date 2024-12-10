79cf2ab26be941f9bd7404e0042c94182221dd1b
avindhawale <avin.dhawale@gmail.com>
2024-12-10 13:32:20 +0530

    user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,4 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'angular18-practice';
-}
+export class AppComponent {}
